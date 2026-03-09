ff466907cd0de7a6c83a9ca33ae1a10b648772eb
mahmudabbas0 <mahmodabas070@gmail.com>
2026-03-09 16:14:40 +0300

    add localization

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+
+        
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
