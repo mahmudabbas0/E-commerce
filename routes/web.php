fa6422d5f5b8ece937292f0058be8b0bb639dd9c
mahmudabbas0 <mahmodabas070@gmail.com>
2026-03-10 11:08:24 +0300

    fix lang

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,12 @@ Route::group(
         'prefix' => LaravelLocalization::setLocale(),
         'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
     ], function(){
-
+        
+    Route::get('/test', function () {
+        return view('dashboard.welcome');
+    });
 
 });
 
 
-Route::get('/test', function () {
-    return view('dashboard.welcome');
-});
+
