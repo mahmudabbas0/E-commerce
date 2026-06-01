ecf5ac5e61aa259bf2cf2ea0b671ec7987df81c5
mahmudabbas0 <mahmodabas070@gmail.com>
2026-06-01 18:05:35 +0300

    -

--- database/migrations/2026_03_10_081725_create_coupons_table.php
+++ database/migrations/2026_03_10_081725_create_coupons_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('coupons', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique();
-            $table->decimal('discount')->nullable();
+           // $table->decimal('discount')->nullable();
             $table->integer('discount_percentage')->default(0);
-            $table->date('expiry_date')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
             $table->integer('limit')->nullable();
             $table->integer('time_used')->default(0);
             $table->boolean('is_active')->default(1);
